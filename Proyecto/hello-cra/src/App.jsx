652f810fa69c8e4c1e7b90ea8f19990ed7000772
VictorYugo <vhs220796@gmail.com>
2020-12-10 19:22:52 -0500

    formularios, botones y css

--- Proyecto/hello-cra/src/App.jsx
+++ Proyecto/hello-cra/src/App.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 //import Form from './Components/Form'
-import Refs from './components/Refs'
+import Refs from './components/Refs';
+import Button from './components/Button/Button';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Refs></Refs>
+        <Button></Button>
       </div>
     );
   }
---
+++ Proyecto/hello-cra/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './button.scss'
+
+const Button = () => {
+    return(
+        <button className='button'>Click Aqui</button>
+    )
+}
+
+export default Button;
