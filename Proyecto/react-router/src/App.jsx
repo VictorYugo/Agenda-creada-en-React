565f4ef7534270b53ef9a79ae07ecda4e6e343f7
VictorYugo <vhs220796@gmail.com>
2021-01-04 18:53:18 -0500

    Usando Reac Router DOM

---
+++ Proyecto/react-router/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import User from './components/User';
+
+const App = () => {
+
+    return (
+        <Router>
+            <Route exact path='/' component={Home}/>
+            <Route path='/user' component={User}/>
+        </Router>
+    )
+}
+
+export default App;
